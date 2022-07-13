fbec66b48ccab7ae49a3d59e1e831df5e9bd2723
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2022-07-13 10:56:25 -0300

    Update musica.dart

--- lib/model/musica.dart
+++ lib/model/musica.dart
@@ -27,3 +27,29 @@ class Musica {
     return map;
   }
 }
+
+
+class Musica {
+  int? id;
+  String? nome;
+  String? descricao;
+  String? data;
+
+  Musica({this.id, this.nome, this.descricao, this.data});
+
+  Musica.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    nome = json['nome'];
+    descricao = json['descricao'];
+    data = json['data'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['nome'] = this.nome;
+    data['descricao'] = this.descricao;
+    data['data'] = this.data;
+    return data;
+  }
+}
