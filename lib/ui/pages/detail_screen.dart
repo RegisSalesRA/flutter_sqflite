36d42558cfb6fd3c73aa89b0426edc9b3d78c4eb
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2022-09-22 10:27:06 -0300

    Detail Pages Updated

--- lib/ui/pages/detail_screen.dart
+++ lib/ui/pages/detail_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sqlite/helpers/helpers.dart';
-//import 'package:sqflite/utils/utils.dart';
-//import '../../helpers/helpers.dart';
 import '../../model/model.dart';
-//import '../../widgets/widgets.dart';
 
 class DetailScreen extends StatelessWidget {
   final Music music;
@@ -15,115 +12,118 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        iconTheme: const IconThemeData(color: Colors.black),
-        title: const Text(
-          "Musica nome",
+        iconTheme: const IconThemeData(color: Palette.primaryColorDark),
+        title: Text(
+          music.title!,
           style: TextStyle(color: Palette.primaryColorDark),
         ),
         centerTitle: true,
         elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ))
+        ],
       ),
       body: SafeArea(
           child: Stack(
         children: [
-          Column(
-            children: [
-              const SizedBox(
-                height: 150,
-              ),
-              Container(
-                height: 250,
-                width: double.infinity,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(
-                    'assets/images/backDetailPageImage.jpg',
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            height: 400,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+          Positioned(
+            top: size.height * 0.15,
+            bottom: size.height * 0.30,
+            right: 0,
+            left: 0,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: Image.asset(
-                'assets/images/detailPageImage.png',
-                fit: BoxFit.cover,
-                width: double.infinity,
+                'assets/images/backDetailPageImage.jpg',
+                fit: BoxFit.fill,
               ),
             ),
           ),
           Positioned(
-              top: 250,
-              right: 30,
-              bottom: 0,
-              left: 30,
-              child: Column(
-                children: [
-                  Container(
-                    height: 50,
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20)),
-                        color: Colors.white),
-                    child: const Center(
-                        child: Text(
-                      "Album Dados Aqui",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Palette.primaryColorDark),
-                    )),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.white,
-                      child: SingleChildScrollView(
-                        physics: const BouncingScrollPhysics(),
-                        scrollDirection: Axis.vertical,
-                        child: Container(
-                          color: Colors.white,
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            children: [
-                              Container(
-                                width: double.infinity,
-                                decoration: const BoxDecoration(
-                                  color: Colors.white,
-                                ),
-                                child: Column(children: const [
-                                  Text(
-                                    "Teste",
-                                    style: TextStyle(
-                                        color: Palette.primaryColorDark),
-                                  )
-                                ]),
-                              )
-                            ],
+            top: size.height * 0.05,
+            bottom: size.height * 0.25,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: size.height * 0.50,
+              padding: const EdgeInsets.all(20),
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/images/detailPageImage.png',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: size.height * 0.40,
+            right: 30,
+            bottom: 0,
+            left: 30,
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  child: Container(
+                    color: Palette.greyShadeLight,
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: size.height * 0.40,
+                          decoration: BoxDecoration(
+                            color: Palette.greyShadeLight,
                           ),
-                        ),
-                      ),
+                          child: Column(children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Palette.greyShadeLight,
+                              ),
+                              padding: const EdgeInsets.only(bottom: 8),
+                              child: Center(
+                                  child: Text(
+                                " '${music.title}' ",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Palette.primaryColorDark),
+                              )),
+                            ),
+                            Expanded(
+                                child: SingleChildScrollView(
+                              physics: const BouncingScrollPhysics(),
+                              child: Text(
+                                music.description!,
+                                style: const TextStyle(
+                                    color: Palette.primaryColorDark),
+                              ),
+                            ))
+                          ]),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ))
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       )),
     );
   }
 }
-
-/*
-
-
-                */
